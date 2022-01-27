5e1bd7044747ba0f4d86226c5635f0dd190014a4
Mitch Treece <mitchtreece@me.com>
2022-01-26 16:04:40 -0800

    Updated function name

--- Spider/Classes/Async/UISpider+Async.swift
+++ Spider/Classes/Async/UISpider+Async.swift
@@ -15,7 +15,7 @@ public extension UISpider where T: ImageView {
     /// - parameter url: The image's URL.
     /// - parameter placeholder: A placeholder image to assign to the current image view while the image is being fetched; _defaults to nil_.
     /// - parameter cacheImage: Flag indicating if the fetched image should be cached; _defaults to true_.
-    /// - returns: A nullable image result.
+    /// - returns: An optional image result.
     ///
     /// The caller is responsible for assigning the image to the image view.
     func setImage(_ url: URLRepresentable,
@@ -37,9 +37,9 @@ public extension UISpider where T: ImageView {
     /// - returns: An image result.
     ///
     /// The caller is responsible for assigning the image to the image view.
-    func setImage(_ url: URLRepresentable,
-                  placeholder: Image? = nil,
-                  cacheImage: Bool = true) async throws -> (Image, Bool) {
+    func setImageThrowing(_ url: URLRepresentable,
+                          placeholder: Image? = nil,
+                          cacheImage: Bool = true) async throws -> (Image, Bool) {
         
         try await withCheckedThrowingContinuation { c in
             
